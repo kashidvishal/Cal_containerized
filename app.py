3d12c3b7554fc5e60c79edd93b509731f5cbce14
Vishal Kashid <kashidvishal2001@gmail.com>
2024-03-06 21:52:24 +0530

    Simple Calculator Containerized web page Log_1

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    operator = request.form['operator']
+
+    result = None
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        if num2 == 0:
+            return "Error: Division by zero!"
+        result = num1 / num2
+
+    return render_template('result.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
